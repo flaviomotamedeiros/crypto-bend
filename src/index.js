8b0658fff6736a9356fca596d062244c648ace79
flaviomotamedeiros <flaviomotamedeiros@gmail.com>
2022-06-06 11:43:22 -0300

    Adding server port for deployment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(userRouter)
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
